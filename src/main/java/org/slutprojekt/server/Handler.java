30a0e77ff92ab2c24ae6f12ce493e1386858cacf
Simon Zettergren <simon.zettergren@elev.ga.ntig.se>
2025-04-01 21:41:54 +0200

    server side login logic

--- src/main/java/org/slutprojekt/server/Handler.java
+++ src/main/java/org/slutprojekt/server/Handler.java
@@ -1,17 +1,23 @@
 package org.slutprojekt.server;
 
 import org.slutprojekt.shared.SocketConnection;
+import org.slutprojekt.shared.models.LoginForm;
 import org.slutprojekt.shared.models.Message;
+import org.slutprojekt.shared.models.User;
 
 import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.HashMap;
 
 public class Handler implements Runnable {
     private SocketConnection socketConnection;
+    private HashMap<String, String> mockDB;
+    private User user;
 
-    public Handler(Socket socket) throws IOException {
+    public Handler(Socket socket, HashMap<String, String> mockDB) throws IOException {
         socketConnection = new SocketConnection(socket);
+        this.mockDB = mockDB;
     }
 
     @Override
@@ -19,18 +25,82 @@ public class Handler implements Runnable {
         try {
             while (true) {
                 Message message = socketConnection.read();
-                if (message.getMessage().equalsIgnoreCase("exit")) {
-                    socketConnection.close();
-                    System.out.println("A client disconnected");
-                    break;
+                switch (message.getMessage()) {
+                    case "login":
+                        if (!(message.getData() instanceof LoginForm)) {
+                            //TODO: respond with an error message to the client instead of crashing
+                            throw new IllegalStateException("Wrong datatype for the command");
+                        }
+                        login((LoginForm) message.getData());
+                        break;
+                    case "signup":
+                        if (!(message.getData() instanceof LoginForm)) {
+                            //TODO: respond to the client instead of crashing
+                            throw new IllegalStateException("Wrong datatype for the command");
+                        }
+                        signup((LoginForm) message.getData());
+                        break;
+                    default:
+                        //TODO: respond to the client instead of crashing
+                        throw new IllegalStateException("Unknown command");
                 }
-                socketConnection.write(message);
-                System.out.println("Echoed: " + message.getMessage());
+
+                // old stuff, might be wanted for testing
+                //if (message.getMessage().equalsIgnoreCase("exit")) {
+                //    socketConnection.close();
+                //    System.out.println("A client disconnected");
+                //    break;
+                //}
+                //socketConnection.write(message);
+                //System.out.println("Echoed: " + message.getMessage());
             }
         } catch (SocketException e) {
             System.out.println("Lost connection to a client");
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    //TODO: replace all exceptions with messages to the client
+    private void login(LoginForm loginForm) {
+        // Username and password must exist
+        if (loginForm.getUsername() == null || loginForm.getPassword() == null ||
+                loginForm.getUsername().isBlank() || loginForm.getPassword().isBlank()) {
+            throw new IllegalStateException("Username and password are required");
+        }
+
+        // DB must contain the user that is being logged in to
+        if (!mockDB.containsKey(loginForm.getUsername())) {
+            throw new IllegalStateException("Username not found");
+        }
+
+        // Validate password
+        if (!mockDB.get(loginForm.getUsername()).equals(loginForm.getPassword())) {
+            throw new IllegalStateException("Password does not match");
+        }
+
+        // Login to the user
+        //TODO: id should not always be zero, correct implementation when real db exists
+        user = new User(0, loginForm.getUsername());
+    }
+
+    //TODO: replace all exceptions with messages to the client
+    private void signup(LoginForm signupForm) {
+        // Username and password must exist
+        if (signupForm.getUsername() == null || signupForm.getPassword() == null ||
+                signupForm.getUsername().isBlank() || signupForm.getPassword().isBlank()) {
+            throw new IllegalStateException("Username and password are required");
+        }
+
+        // Don't allow duplicate usernames
+        if (mockDB.containsKey(signupForm.getUsername())) {
+            throw new IllegalStateException("Username already exists");
+        }
+
+        // Create the user in the db
+        mockDB.put(signupForm.getUsername(), signupForm.getPassword());
+
+        // Login to the user
+        user = new User(0, signupForm.getUsername());
     }
 }
--- src/main/java/org/slutprojekt/server/Server.java
+++ src/main/java/org/slutprojekt/server/Server.java
@@ -1,11 +1,10 @@
 package org.slutprojekt.server;
 
-import org.slutprojekt.shared.SocketConnection;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -18,13 +17,17 @@ public class Server implements AutoCloseable {
     }
 
     public void run() throws IOException {
+        // Create and start the server
         serverSocket.bind(new InetSocketAddress("127.0.0.1", 8080));
         System.out.println("Server started");
 
+        //TODO: Replace with an sqlite database
+        HashMap<String, String> mockDB = new HashMap<>();
+
+        // Continuously accept clients and dispatch threads to handle them
         while (true) {
-            System.out.println("Waiting for connection");
             Socket socket = serverSocket.accept();
-            pool.execute(new Handler(socket));
+            pool.execute(new Handler(socket, mockDB));
             System.out.println("Connected with a client");
         }
     }
