87b95572aa52f0bdb99dc8dcdb83e6882ea515e5
Simon Zettergren <simon.zettergren@elev.ga.ntig.se>
2025-04-01 10:14:40 +0200

    A little more robust server

--- src/main/java/org/slutprojekt/server/Handler.java
+++ src/main/java/org/slutprojekt/server/Handler.java
@@ -5,6 +5,7 @@ import org.slutprojekt.shared.models.Message;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class Handler implements Runnable {
     private SocketConnection socketConnection;
@@ -20,12 +21,14 @@ public class Handler implements Runnable {
                 Message message = socketConnection.read();
                 if (message.getMessage().equalsIgnoreCase("exit")) {
                     socketConnection.close();
-                    System.out.println("Stopping thread");
+                    System.out.println("A client disconnected");
                     break;
                 }
                 socketConnection.write(message);
                 System.out.println("Echoed: " + message.getMessage());
             }
+        } catch (SocketException e) {
+            System.out.println("Lost connection to a client");
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
