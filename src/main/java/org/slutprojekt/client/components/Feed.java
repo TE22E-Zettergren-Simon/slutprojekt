ccd7c86a7829aad27c9e242e0e7b9a81bfb345dd
Simon Zettergren <zettergren.simon@gmail.com>
2025-04-15 16:01:14 +0200

    scrollable feed

--- src/main/java/org/slutprojekt/client/components/Feed.java
+++ src/main/java/org/slutprojekt/client/components/Feed.java
@@ -1,25 +1,27 @@
 package org.slutprojekt.client.components;
 
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 
 import java.util.ArrayList;
 
-public class Feed<T extends Node> extends VBox {
+public class Feed<T extends Node> extends ScrollPane {
+    private VBox vbox = new VBox();
     private ArrayList<T> items = new ArrayList<>();
 
-    public void addMultiple(ArrayList<T> newItems) {
-        items.addAll(newItems);
-        getChildren().addAll(newItems);
+    public Feed() {
+        setContent(vbox);
     }
 
     public void addTop(T newItem) {
         items.addFirst(newItem);
-        getChildren().addFirst(newItem);
+        vbox.getChildren().addFirst(newItem);
     }
 
     public void addBottom(T newItem) {
         items.add(newItem);
-        getChildren().add(newItem);
+        vbox.getChildren().add(newItem);
     }
 }
