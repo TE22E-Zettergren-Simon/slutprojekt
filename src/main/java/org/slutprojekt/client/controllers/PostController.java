ba3dbe4559ce20fb255c541ca1b7bdc116dc3d2d
Simon Zettergren <zettergren.simon@gmail.com>
2025-04-15 15:42:50 +0200

    get comments from server in client

--- src/main/java/org/slutprojekt/client/controllers/PostController.java
+++ src/main/java/org/slutprojekt/client/controllers/PostController.java
@@ -4,8 +4,13 @@ import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
 import org.slutprojekt.client.FXMLUtils;
 import org.slutprojekt.client.components.*;
+import org.slutprojekt.client.state.ConnectionHolder;
 import org.slutprojekt.client.state.CurrentPostHolder;
 import org.slutprojekt.shared.models.*;
+
+import java.io.IOException;
+import java.net.SocketException;
+import java.util.ArrayList;
 
 public class PostController {
     @FXML
@@ -15,8 +20,7 @@ public class PostController {
 
     @FXML
     private void initialize() {
-        //TODO: Get data from server
-
+        // Get the current post and show it
         Post post = CurrentPostHolder.getInstance().getPost();
         PostComponent postComponent;
         if (post instanceof LongPost) {
@@ -26,6 +30,32 @@ public class PostController {
         }
         postComponent.extend();
         root.getChildren().add(1, postComponent);
+
+        // Get the post's comments
+        try {
+            Message<Post> out = new Message<>("get comments", post);
+            ConnectionHolder.getInstance().getSocketConnection().write(out);
+            Message in = ConnectionHolder.getInstance().getSocketConnection().read();
+
+            //TODO: Better error handling, write something to the user in the window
+            if (in == null) {
+                return;
+            }
+            if (!(in.getData() instanceof ArrayList)) {
+                System.out.println(in.getData());
+                return;
+            }
+
+            // Add the comments to the feed
+            for (Comment comment : (ArrayList<Comment>) in.getData()) {
+                CommentComponent commentComponent = new CommentComponent(comment);
+                feed.addBottom(commentComponent);
+            }
+        } catch (SocketException e) {
+            FXMLUtils.loadNewView("views/no-connection.fxml", feed.getScene());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
